748d4e8541b33f05ad32a1fba173739d56fd48a1
moniet <moniet.sawhney@gmail.com>
2019-05-14 13:44:35 +0100

    added method to get film urls

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,24 +2,32 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+def get_info(api_hash, char_name)
+  api_hash["results"].each do |hash|
+    hash.each do |key, val| #binding.pry
+      if val.downcase == char_name.downcase
+        return hash["films"]  #returning array of film urls
+      end
+    end
+  end
+end
+
 def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  films_arr = get_info(response_hash, character_name)
+  new_films_arr = []
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `print_movies`
-  #  and that method will do some nice presentation stuff like puts out a list
-  #  of movies by title. Have a play around with the puts with other info about a given film.
+  films_arr.each do |url|
+    film_info = RestClient.get(url)
+    film_hash = JSON.parse(film_info)
+    new_films_arr << film_hash
+  end
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+
 end
 
 def show_character_movies(character)
@@ -27,7 +35,10 @@ def show_character_movies(character)
   print_movies(films)
 end
 
+#get_character_movies_from_api("Luke Skywalker")
+#print_movies(get_character_movies_from_api)
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+get_character_movies_from_api("Luke Skywalker")
